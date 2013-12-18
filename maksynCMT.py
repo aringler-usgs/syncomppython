c32802eec132a987d7eb84b9f44adb516590f368
aringler-usgs <aringler@usgs.gov>
2013-12-18 09:41:50 -0700

    Started to fix the comments so the code can be more easily used

--- maksynCMT.py
+++ maksynCMT.py
@@ -1,63 +1,114 @@
 #!/usr/bin/env python
 
+######################
+#UpdatesynCMT.py
+#Adam Ringler
+#
+#This code takes the CMTmineos solutions and produces synthetics.  This
+#code assumes you have an installation of MINEOS and it is setup with
+#path information
+#####################
+
 import os
 import glob
 import sys
 import shutil
 
-debug = bool(1)
+debug = False
 
+#Here is where your CMTs are as created by updateCMT.py
 cmtdirpath = '/home/aringler/syncomppython'
+
+#Here is where the MINEOS code resides
 codepath = '/home/aringler/syncomppython'
-eventlist = glob.glob(cmtdirpath + '/2*/*') + glob.glob(cmtdirpath + '/1*/*')
-currdir = os.getcwd()
-ind=0;
-if debug == bool(1):
-	print eventlist
-	print 'The current directory is:' + currdir
-for event in eventlist:
-	ind += 1
-	print 'On event ' + str(ind) + ' of ' + str(len(eventlist)) 
-	if debug == bool(1):
-		print event
-	if len(os.listdir(event)) == 2:
-		if debug == bool(1):
-			print 'No synthetics for this event'
-		
-		parafile = open('parameter_file','w')
-		parafile.write(codepath + '/auxfiles/longNEW\n')
-		parafile.write(codepath + '/auxfiles/modefiles/db_list\n')
-		parafile.write(event + '/currCMTmineos\n')
-		parafile.write('0 260\n')
-		parafile.write('8000\n')
-		parafile.write('green\n')
-		parafile.close()
-		os.system(codepath + '/bin/green < parameter_file')
-		os.remove('parameter_file')
-		shutil.copy2(codepath + '/auxfiles/longNEW.site', currdir + '/green.site')			
-		shutil.copy2(codepath + '/auxfiles/longNEW.sitechan', currdir + '/green.sitechan')
-		
-		if os.path.exists(currdir + 'Syndat.wfdisc'):
+
+def rungreen(event,codepath,currdir):
+#This function runs the greens functions using the codepath and the event info
+
+#Here is the length of the time series we are using
+	tslen = str(8000)
+
+#Lets setup the parameter file to run the Greens functions		
+	parafile = open('parameter_file','w')
+	parafile.write(codepath + '/auxfiles/longNEW\n')
+	parafile.write(codepath + '/auxfiles/modefiles/db_list\n')
+	parafile.write(event + '/currCMTmineos\n')
+
+#Lets do the first 260 modes
+	parafile.write('0 260\n')
+
+	parafile.write(tslen + '\n')
+	parafile.write('green\n')
+	parafile.close()
+
+#We can now feed the code into the greens program
+	os.system(codepath + '/bin/green < parameter_file')
+	os.remove('parameter_file')
+	shutil.copy2(codepath + '/auxfiles/longNEW.site', currdir + '/green.site')			
+	shutil.copy2(codepath + '/auxfiles/longNEW.sitechan', currdir + '/green.sitechan')
+	return;
+
+
+def runsyndat(event,codepath,currdir):
+#This function runs the synthetics using syndat from Mineos
+	if os.path.exists(currdir + 'Syndat.wfdisc'):
 #Need to clean up the Syndat
 			print 'Need to remove Syndat\n'
-		parafile = open('parameter_file','w')
-		parafile.write(event + '/currCMTmineos\n')
-		parafile.write('0\n')
-		parafile.write('green\n')
-		parafile.write('Syndat\n')
-		parafile.write('0\n')
-		parafile.close()
-		os.system(codepath + '/bin/syndat < parameter_file')
-		os.remove(currdir + '/parameter_file')
-		shutil.copy2(codepath + '/auxfiles/longNEW.site', currdir + '/Syndat.site')			
-		shutil.copy2(codepath + '/auxfiles/longNEW.sitechan', currdir + '/Syndat.sitechan')
-		os.system(codepath + '/bin/creat_origin ' + event + '/currCMTmineos Syndat')
+	parafile = open('parameter_file','w')
+	parafile.write(event + '/currCMTmineos\n')
+	parafile.write('0\n')
+	parafile.write('green\n')
+	parafile.write('Syndat\n')
+	parafile.write('0\n')
+	parafile.close()
+	os.system(codepath + '/bin/syndat < parameter_file')
+	os.remove(currdir + '/parameter_file')
+	shutil.copy2(codepath + '/auxfiles/longNEW.site', currdir + '/Syndat.site')			
+	shutil.copy2(codepath + '/auxfiles/longNEW.sitechan', currdir + '/Syndat.sitechan')
+	os.system(codepath + '/bin/creat_origin ' + event + '/currCMTmineos Syndat')	
+	return;
+
+
+
+
+
+
+
+#Lets get all of the events for years starting with 1 and 2
+eventlist = glob.glob(cmtdirpath + '/2*/*') + glob.glob(cmtdirpath + '/1*/*')
+currdir = os.getcwd()
+
+if debug:
+	print eventlist
+	print 'The current directory is:' + currdir
+
+#Time to loop through all of the events and create synthetics for them
+for ind, event in enumerate(eventlist):
+	print 'On event ' + str(ind + 1) + ' of ' + str(len(eventlist)) 
+	
+	if debug:
+		print event
+	
+#Okay we have an event if there are more than 2 files in the directory
+#This should eventually be made more robust
+	if len(os.listdir(event)) == 2:
+		if debug:
+			print 'No synthetics for this event'
+
+#Run the greens functions		
+		rungreen(event,codepath,currdir)
+
+#Now we run the syndat piece of Mineos		
+		runsyndat(event,codepath,currdir)
 		
+#Lets convert the synthetics to SAC format
 		os.system(codepath + '/bin/cucss2sac Syndat Syns')
 #Time to clean up stuff
 		os.system('rm -r ' + currdir + '/Syndat.*')
 		os.system('rm -r ' + currdir + '/green.*')
 		synall = glob.glob(currdir + '/Syns/*.SAC')
+
+#Here we are changing from H to X to get no network		
 		for syncur in synall:
 			os.rename(syncur, syncur.replace(' ',''))
 			syncur=syncur.replace(' ','')
@@ -65,12 +116,15 @@ for event in eventlist:
 			syncurchan = syncurchan.split('.')
 			syncurchan[6] = syncurchan[6].replace('H','X')
 			syncurchan = syncurchan[5] + '.XX.' + syncurchan[6] + '.modes.sac'
-			if debug == bool(1):
+			if debug:
 				print 'Old synthetic:' + syncur
 				print 'New synthetic:' + syncurchan
 				print 'Move to location:' + event + '/' + syncurchan
 			os.system('mv ' + syncur + ' ' + event + '/' + syncurchan)
 		os.system('rm -r ' + currdir + '/Syns')
+
+#Here we setup our process_syn.pl code to do the pre-processing
+#This should be removed and done in python later as it is really sloppy
 		synprostr = '-S -m ' + event + '/CMTSOLUTION '
 		synprostr = synprostr + '-s 1.0 -l 0/4000 -t 40/400 -x proc ' 
 		synprostr = synprostr + event + '/*modes.sac '	
--- updateCMT.py
+++ updateCMT.py
@@ -1,44 +1,75 @@
 #!/usr/bin/env python
 
+######################
+#UpdateCMT.py
+#Adam Ringler
+#
+#This code gets CMTs from Lamont and sets up a directory structure.
+#The code pulls ndk format CMTs and switches them to CMTSOLUTION and 
+#mineos format.
+#####################
 import os
 import sys
 import datetime
 import math
 
-debug = bool(0)
+#Debug flag for verbose output during run
+debug = True
 
-cmtdirpath = '/home/aringler/syncomppython'
-codepath ='/home/aringler/syncomppython'
+#Here we have the option of grabbing the large historic file
+backfill = True
+
+#Here is the location for the directory structure setup
+cmtdirpath = '/home/aringler/synthetics/syncompdev'
+codepath ='/home/aringler/synthetics/syncompdev/syncomppython'
+
+#Here is the minimum magnitude to use for the events
 minmag= 6.5
 
-#Download the latest CMT files
+#Download the latest CMT files and place them in the croncode location
 currdir = os.getcwd()
 if not os.path.exists(codepath + '/croncode'):
 	os.mkdir(codepath + '/croncode')
 os.chdir(codepath + '/croncode')
-os.system('wget -N http://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/NEW_QUICK/qcmt.ndk')
+#This only gets the most recent quick CMTs for the older CMTS you need to backfill
+if backfill:
+	os.system('wget -N http://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/jan76_dec10.ndk')
+else:
+	os.system('wget -N http://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/NEW_QUICK/qcmt.ndk')
 os.chdir(currdir)
 
 #Read in the CMT file
-qcmt = open(codepath + '/croncode/qcmt.ndk','r')
+if backfill:
+	qcmt = open(codepath + '/croncode/jan76_dec10.ndk','r')
+else:
+	qcmt = open(codepath + '/croncode/qcmt.ndk','r')
+
+#Lets loop through the code reading 5 lines at a time and pull apart the ndk event info
 while True:
 	line1 = qcmt.readline()
 	line2 = qcmt.readline()
 	line3 = qcmt.readline() 
 	line4 = qcmt.readline()
 	line5 = qcmt.readline()
+
+#If we dont have a line 2 then we are done with the file
 	if not line2: break
 	mag1 = float(line1[49:51])	
 	mag2 = float(line1[52:55])
 	mag = max(mag1,mag2)
+
 #Lets only look at CMTs with a certain magnitude
 	if mag >= minmag:
-		if debug == bool(1):
+		if debug:
 			print 'Magnitude:' + str(mag)
+
 #Transform into MINEOS format		
 		CMTdate = datetime.datetime(int(line1[5:9]), int(line1[10:12]), int(line1[13:16]),0,0,0)
-		if debug == bool(1):
+		if debug:
 			print 'Here is the day of year ' + str(CMTdate.timetuple()[7])
+
+#Now we format the lines from the line info
+#This should probably be split.  However it is such a messy format we will stick to this
 		line1CMT = line2[0:14].strip() +  ' ' + line1[5:9] + ' ' + str(CMTdate.timetuple()[7]) + ' '
 		line1CMT = line1CMT + line1[16:18] + ' ' + line1[19:21] + ' ' + line1[22:26].ljust(5,'0') + ' '
 		line1CMT = line1CMT + line1[28:33] + ' ' + line1[34:41] + ' ' + line1[42:47].strip() + ' 1.0 '
@@ -46,17 +77,19 @@ while True:
 		line4 = line4.split()
 		line3 = line3.split()
 		CMTexp = line4[0]
-		if debug == bool(1):
+		if debug:
 			print 'CMTexp:' + CMTexp
+#Pull out the CMT components
 		Mrr = float(line4[1])
 		Mtt = float(line4[3])
 		Mpp = float(line4[5])
 		Mrt = float(line4[7])
 		Mrp = float(line4[9])
 		Mtp = float(line4[11])
+#Lets get the moment magnitude and split this out
 		M0 = math.sqrt(Mrr**2 + Mtt**2 + Mpp**2 + 2*(Mrp**2 + Mtp**2 + Mrt**2))/math.sqrt(2)
 		M0 = str('{0:.3f}'.format(M0)) + 'e' + CMTexp
-		if debug == bool(1):
+		if debug:
 			print line1CMT 
 			print 'M0=' + M0
 		line5 = line5.split()
@@ -72,14 +105,19 @@ while True:
 		if not os.path.exists(cmtdire):
 			os.mkdir(cmtdire)
 
+#Here we write the Mineos solution and close the file
 		CMTmineos = open(cmtdire  + '/currCMTmineos','w')
-		CMTSol = open(cmtdire + '/CMTSOLUTION','w')
 		CMTmineos.write(line1CMT)
 		CMTmineos.close()
+
+#Lets also open the CMTSOLUTION and get ready to write this result
+		CMTSol = open(cmtdire + '/CMTSOLUTION','w')
 		line1CMTSol = line1[0:3] + ' ' + str(int(line1[5:9])) + ' ' + line1[10:12]+ ' ' + line1[13:16] + ' '
 		line1CMTSol = line1CMTSol + line1[16:18] + ' ' + line1[19:21] + ' ' + line1[22:26].ljust(5,'0')
 		line1CMTSol = line1CMTSol + ' ' + line1[27:33] + ' ' + line1[34:41] + ' ' + line1[42:47] + ' '
 		line1CMTSol = line1CMTSol + str(format(mag1,'1.1f')) + ' ' + str(format(mag2,'1.1f'))+ '\n'
+
+#Here we writ the CMTSOLUTION format
 		CMTSol.write(line1CMTSol)
 		CMTSol.write('event name:' + line2[1:14].rjust(18,' ') + '\n')
 		CMTSol.write('time shift:' + (line3[1].rjust(9,' ')).ljust(12,'0') + '\n')
